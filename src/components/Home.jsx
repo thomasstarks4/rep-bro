824f868b14a69649ce5fd288b9d1372df28a91dc
thomasstarks4 <thomas.a.starks4@gmail.com>
2024-08-05 23:14:57 -0400

    initial logger draft. committing to save work.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
       <div className="home-container">
         <div className="main-header">
           <div className="flex j-center">
-            <h2 className="home-header">
+            <h1 className="home-header">
               RepBro: Your All-in-One Workout Tracking App
-            </h2>
+            </h1>
           </div>
           <img className="home-image" src={appLogo} alt="App Logo" />
         </div>
@@ -20,7 +20,7 @@ function Home() {
           gym sessions, RepBro has got you covered.
         </h3>
         <div className="list-container">
-          <h3 className="t-center">Track Your Workouts with Ease</h3>
+          <h2 className="t-center">Track Your Workouts with Ease</h2>
           <ul>
             <li>
               <strong>Reps & Sets:</strong> Log each rep and set effortlessly.
@@ -36,7 +36,7 @@ function Home() {
           </ul>
         </div>
         <div className="list-container">
-          <h3 className="t-center">Plan and Schedule Your Workouts</h3>
+          <h2 className="t-center">Plan and Schedule Your Workouts</h2>
           <ul>
             <li>
               <strong>Printable Workout Plans:</strong> Design your workout
@@ -49,7 +49,7 @@ function Home() {
           </ul>
         </div>
         <div className="list-container">
-          <h3 className="t-center">Stay Organized and Motivated</h3>
+          <h2 className="t-center">Stay Organized and Motivated</h2>
           <ul>
             <li>
               <strong>Check Off Completed Workouts:</strong> Simply check off
--- src/components/Logger.jsx
+++ src/components/Logger.jsx
@@ -1,30 +1,58 @@
+import "./css/Logger.css";
+import { useState } from "react";
+
 function Logger() {
+  const getCurrentDayAndDate = () => {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const time = new Date();
+    const dayName = daysOfWeek[time.getDay()];
+    const currentDate = time.toLocaleDateString();
+
+    return `${dayName}, ${currentDate}`;
+  };
+  const [workoutInfo, setWorkoutInfo] = useState({
+    name: `${getCurrentDayAndDate()}'s Workout`,
+    typeName: "Chest",
+    formCompleted: false,
+  });
   return (
-    <div className="logger-container">
-      <h1>Logger</h1>
-      <div className="inputs-container">
-        {/* Exercise Type Input */}
-        <div className="input-container">
-          <label htmlFor="ExerciseType">Exercise Type</label>
-          <input name="ExerciseType" type="text" className="exercise-input" />
-        </div>
-        {/* Number Of Reps Input */}
-        <div className="input-container">
-          <label htmlFor="NumberOfReps" className="exercise-label">
-            Number Of Reps
-          </label>
-          <input name="NumberOfReps" type="text" className="exercise-input" />
-        </div>
-        {/* Weight Input */}
-        <div className="input-container">
-          <label htmlFor="Weight" className="Weight">
-            Weight
-          </label>{" "}
-          <input type="checkbox" />
-          <input name="Weight" type="text" className="exercise-input" />
+    <>
+      <h1 className="t-center white">Logger</h1>
+      <div className="logger-container">
+        <h1 className="t-center white">{workoutInfo.name}</h1>
+        <h2 className="t-center white">{workoutInfo.typeName} Day</h2>
+        <div className="inputs-container">
+          {/* Exercise Type Input */}
+          <div className="input-container">
+            <label htmlFor="ExerciseName">Exercise Name</label>
+            <input name="ExerciseName" type="text" className="exercise-input" />
+          </div>
+          {/* Number Of Reps Input */}
+          <div className="input-container">
+            <label htmlFor="NumberOfReps" className="exercise-label">
+              Number Of Reps
+            </label>
+            <input name="NumberOfReps" type="text" className="exercise-input" />
+          </div>
+          {/* Weight Input */}
+          <div className="input-container">
+            <label htmlFor="Weight" className="Weight">
+              Weight
+            </label>{" "}
+            <input name="Weight" type="text" className="exercise-input" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
